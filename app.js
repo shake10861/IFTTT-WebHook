262c3b2b230818759878d2d1d8cb27ee1ac16942
Jake Anderson <jake.anderson@cerner.com>
2015-03-24 11:20:32 -0500

    IFTTT Wordpress Hack Test

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+var port = 80;
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var webhook = require('express-ifttt-webhook');
+var fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 80;
+app.use(webhook(function (json, done) {
+    fs.writeFile("./IFTTT_IN.txt", JSON.stringify(json, null, 2));
+    done();
+}));
+
 
 var router = express.Router();
-
 router.get("/", function (req, res) {
     res.json({ message: "This REST service is online." });
 });
