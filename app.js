c6c6fa523836ef4c9bcbba3678b0b10705e6b060
Jake Anderson <jake.anderson@cerner.com>
2015-03-24 10:57:43 -0500

    REST TEST

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function (req, res) {
-    res.send("TEST WORKED!");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.evn.PORT || 80;
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    res.json({ message: "This REST service is online." });
 });
 
+app.use("/api", router);
+
+app.listen(port);
+console.log("Magic happens on port " + port);
